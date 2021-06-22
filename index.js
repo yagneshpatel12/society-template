1fc70c6641cd81b4e98e2665bb9eaae0b6f9d9c8
yagnesh <yagnesh6202patel@gmail.com>
2021-06-22 22:01:15 +0530

    making navbar responsive

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 let rightSideMenu = document.getElementsByClassName('rightSide')[0];
 let line = document.getElementsByClassName('line');
 rightSideMenu.style.maxHeight = '0px'
-let hamburger
 function togglemenu(){
     if(rightSideMenu.style.maxHeight == '0px'){
         rightSideMenu.style.maxHeight = '400px';
-       line[0].setAttribute('id','line1');
-       line[1].setAttribute('id','line2');
-       line[2].setAttribute('id','line3');
+        for(let i=0;i<3;i++)
+            line[i].setAttribute('id',`line${i+1}`);
     }
     else{
         rightSideMenu.style.maxHeight ='0px'
-       line[0].removeAttribute('id');
-       line[1].removeAttribute('id');
-       line[2].removeAttribute('id');
+        for(let i=0;i<3;i++)
+        line[i].removeAttribute('id',`line${i+1}`);
     }
 }
